b9f5e1cabf53dd52bd5d6ab7756bfe9204634975
Kim <mrsshoes2@gmail.com>
2020-02-15 19:07:35 +0000

    refactored forgotten password test

--- tests/forgotten-password.test.js
+++ tests/forgotten-password.test.js
@@ -1,24 +1,24 @@
-import {Selector} from "testcafe"
 import Navbar from "../page-objects/components/Navbar"
+import LoginPage from "../page-objects/pages/LoginPage"
+import ForgottenPasswordPage from "../page-objects/pages/ForgottenPasswordPage"
 
 const navbar = new Navbar()
+const loginPage = new LoginPage()
+const forgottenPasswordPage = new ForgottenPasswordPage()
 
 fixture `Send forgotten password test`
 .page `http://zero.webappsecurity.com/index.html`
 
 test("User can request new password to be sent to their email", async t => {
-    // Get selectors
-    const linkToPassword = Selector("a").withText("Forgot your password ?")
-    const emailInput = Selector("#user_email")
-    const message = Selector("div").innerText
-    
     // Actions
-    await t.click(navbar.signInButton)
-    await t.click(linkToPassword)
-    await t.typeText(emailInput, "test@test.com", {paste: true})
-    await t.pressKey("enter")
+    await t
+        .click(navbar.signInButton)
+        .click(loginPage.linkToPassword)
+        .typeText(forgottenPasswordPage.emailInput, "test@test.com", { paste:true, replace:true })
+        .pressKey("enter")
     
     // Assertion
-    await t.expect(message).contains("test@test.com")
-    await t.expect(emailInput.exists).notOk
+    await t
+        .expect(forgottenPasswordPage.message.innerText).contains("test@test.com")
+        .expect(forgottenPasswordPage.emailInput.exists).notOk
 })
