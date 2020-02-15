eca60ceb133d7cf84bdd7689d57d9cf099066687
Kim <mrsshoes2@gmail.com>
2020-02-15 18:12:12 +0000

    search test refactor

--- page-objects/components/Navbar.js
+++ page-objects/components/Navbar.js
@@ -6,6 +6,12 @@ class Navbar {
         this.searchBar = Selector("#searchTerm")
         this.signInButton = Selector("#signin_button")
     }
+
+    async search(text) {
+        await t
+        .typeText(this.searchBar, text, { paste:true, replace:true })
+        .pressKey("enter")
+    }
 }
 
 export default Navbar
--- tests/search.test.js
+++ tests/search.test.js
@@ -1,22 +1,19 @@
-import {Selector} from "testcafe"
 import Navbar from "../page-objects/components/Navbar"
+import SearchResultPages from "../page-objects/pages/SearchResultsPage"
 
 const navbar = new Navbar()
+const searchResultsPage = new SearchResultPages()
 
 fixture `Search bar test`
 .page `http://zero.webappsecurity.com/index.html`
 
 test("User can search for information using searchbar", async t => {
-    // Selectors
-    const resultsTitle = Selector("h2").innerText
-    const linkText = Selector("div").innerText
-    
     // Actions
-    await t.typeText(navbar.searchBar, "online bank", {paste:true})
-    await t.pressKey("enter")
+    navbar.search("online Bank")
 
     //Assertions
-    await t.expect(resultsTitle).contains("Search Results:")
-    await t.expect(navbar.searchBar.exists).ok()
-    await t.expect(linkText).contains("Zero - Free Access to Online Banking")
+    await t
+        .expect(searchResultsPage.resultsTitle.innerText).contains("Search Results:")
+        .expect(navbar.searchBar.exists).ok()
+        .expect(searchResultsPage.linkText.innerText).contains("Zero - Free Access to Online Banking")
 })
